1520ae0de7280b5d872db8b2b56f1c61fa8002df
marcellusc <marcelluscurt@gmail.com>
2022-02-13 01:08:17 +0100

    init2

---
+++ wp-content/uploads/mailpoet/cache/1e/1e97a0ef6707fc5691cdb1902d2929054a33d53c47be9e73db6dd3f58581606c.php
@@ -0,0 +1,62 @@
+<?php
+
+use MailPoetVendor\Twig\Environment;
+use MailPoetVendor\Twig\Error\LoaderError;
+use MailPoetVendor\Twig\Error\RuntimeError;
+use MailPoetVendor\Twig\Extension\SandboxExtension;
+use MailPoetVendor\Twig\Markup;
+use MailPoetVendor\Twig\Sandbox\SecurityError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedTagError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFilterError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use MailPoetVendor\Twig\Source;
+use MailPoetVendor\Twig\Template;
+
+/* newsletter/templates/blocks/social/block.hbs */
+class __TwigTemplate_3fdd762f8eb6e3e8124e473d3da2264293bf45ce986757ec781d50a6618a1007 extends \MailPoetVendor\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"mailpoet_tools\"></div>
+<div
+    class=\"mailpoet_content mailpoet_social\"
+    data-automation-id=\"socialBlock\"
+    style=\"text-align: {{model.styles.block.textAlign}}\"
+>
+</div>
+<div class=\"mailpoet_block_highlight\"></div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "newsletter/templates/blocks/social/block.hbs";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "newsletter/templates/blocks/social/block.hbs", "C:\\xampp\\htdocs\\wordpress\\wp-content\\plugins\\mailpoet\\views\\newsletter\\templates\\blocks\\social\\block.hbs");
+    }
+}
---
+++ wp-content/uploads/mailpoet/cache/d1/d1bd4e4a95ed466451270dc510c6a4cb89fc05e3cc05c4af4af2f1456a893ced.php
@@ -0,0 +1,60 @@
+<?php
+
+use MailPoetVendor\Twig\Environment;
+use MailPoetVendor\Twig\Error\LoaderError;
+use MailPoetVendor\Twig\Error\RuntimeError;
+use MailPoetVendor\Twig\Extension\SandboxExtension;
+use MailPoetVendor\Twig\Markup;
+use MailPoetVendor\Twig\Sandbox\SecurityError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedTagError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFilterError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use MailPoetVendor\Twig\Source;
+use MailPoetVendor\Twig\Template;
+
+/* newsletter/templates/blocks/posts/settingsSinglePost.hbs */
+class __TwigTemplate_561c1d5faa2495e2d5f1918f29a70cc123518f05f31ed978e395d2ed3d9108ca extends \MailPoetVendor\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"mailpoet_settings_posts_single_post\">
+    <label>
+        <input id=\"mailpoet_select_post_{{ index }}\" class=\"mailpoet_select_post_checkbox\" type=\"checkbox\" class=\"checkbox\" value=\"\" name=\"post_selection\">
+        {{#ellipsis model.post_title 40 '...'}}{{/ellipsis}}
+    </label>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "newsletter/templates/blocks/posts/settingsSinglePost.hbs";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "newsletter/templates/blocks/posts/settingsSinglePost.hbs", "C:\\xampp\\htdocs\\wordpress\\wp-content\\plugins\\mailpoet\\views\\newsletter\\templates\\blocks\\posts\\settingsSinglePost.hbs");
+    }
+}
